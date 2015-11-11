a4ce258bcb1511a9b510d910c9059b0fa12c6d72
kaerluo <1300453233@qq.com>
2015-11-11 11:39:54 +0800

    添加Imd_docter_Bll

---
+++ IBll/User/Imd_docter_Bll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using Model;
+
+namespace IBll
+{
+    public interface Imd_docter_Bll
+    {
+        #region md_docter
+        #region 查询
+
+        md_docter GetDocterById(int id);
+        md_docter LoginUtil(string loginname, string loginpass, string keyvalue, out string error);
+
+        #endregion
+
+        #region 修改
+
+        md_docter UpdateDocter(md_docter info, out string error);
+
+        #endregion
+        #endregion
+    }
+}
