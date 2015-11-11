cf70ce7492b0e9a2b2d06392c541b3f69a51785c
kaerluo <1300453233@qq.com>
2015-11-11 17:10:58 +0800

    提现申请DA修改

--- Dal/User/md_cashdraw_app_DA.cs
+++ Dal/User/md_cashdraw_app_DA.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Model;
+using Model.ConfigClass;
+using Share;
 
-namespace Dal.User
+namespace Dal
 {
-    class md_cashdraw_app_DA
+    public class md_cashdraw_app_DA:DataAccessBase
     {
+        #region 添加
+        public md_cashdraw_app CreateCashdrawApp(md_cashdraw_app info, out string error)
+        {
+            error = string.Empty;
+            try
+            {
+                db.md_cashdraw_app.Add(info);
+                db.SaveChanges();
+                return info;
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+            {
+                error = Share.BaseTool.FormatExceptionMessage(ex);
+                return null;
+            }
+
+        }
+        #endregion
     }
 }
