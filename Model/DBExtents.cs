c43f3c6fcb91e0c64e65f40fb8e813ba045c9589
lichao89 <61640542@qq.com>
2015-11-11 10:42:23 +0800

    出诊相关 DAL

---
+++ Model/DBExtents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Data;
+using AutoMapper;
+using System.Linq.Expressions;
+namespace Model
+{
+    public static class DBExtents
+    {
+        #region initial entity
+        public static void Initial<T>(this T vobj)
+        {
+
+        }
+        #endregion
+    }
+}
