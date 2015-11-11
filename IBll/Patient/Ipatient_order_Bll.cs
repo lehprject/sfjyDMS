9cd6ddb30300e725afa2dee28301e73cb09e0220
kaerluo <1300453233@qq.com>
2015-11-11 17:03:22 +0800

    Ipatient_order_Bll修改

--- IBll/Patient/Ipatient_order_Bll.cs
+++ IBll/Patient/Ipatient_order_Bll.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IBll.Patient
+namespace IBll
 {
-    class Ipatient_order_Bll
+    public interface Ipatient_order_Bll
     {
+        List<patient_order> SearchPatientOrderList(int patient_id, int drid, int service_type, DateTime? createtime,
+       orderbyEnum? orderby, string orderbyCol, int pageIndex, int pageSize, out string error);
     }
 }
