332aa6382434798be6d321bbfa97c5c878bc23d7
kaerluo <1300453233@qq.com>
2015-11-12 15:30:36 +0800

    添加药品BLL，IBLL

---
+++ Bll/MasterData/md_druginfo_Bll.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.MasterData
+{
+    class md_druginfo_Bll
+    {
+    }
+}
--- Dal/MasterData/md_druginfo_DA.cs
+++ Dal/MasterData/md_druginfo_DA.cs
@@ -3,10 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Model;
+using Model.ConfigClass;
+using Share;
 
-namespace Dal.MasterData
+namespace Dal
 {
-    class md_druginfo_DA
+    public class md_druginfo_DA : DataAccessBase
     {
+        public List<md_druginfo> SearchDruginfoList(string drugname, string standard, int producerid, string cust_name, string sub_name,
+            orderbyEnum? orderby, string orderbyCol, int pageIndex, int pageSize, out string error)
+        {
+            error = string.Empty;
+            try
+            {
+                #region Command
+
+                string selectSql = string.Format("select * from md_druginfo WHERE TRUE ");
+
+                StringBuilder conditionSb = new System.Text.StringBuilder();
+
+                List<MySqlParameter> paraList = new List<MySqlParameter>();
+                #endregion
+
+                #region 搜索条件
+                if (!string.IsNullOrEmpty(drugname))
+                {
+                    conditionSb.Append("AND drugname = @drugname");
+                    paraList.Add(new MySqlParameter("drugname", drugname));
+                }
+
+                if (!string.IsNullOrEmpty(standard))
+                {
+                    conditionSb.Append("AND standard = @standard");
+                    paraList.Add(new MySqlParameter("standard", standard));
+                }
+
+                if (producerid > 0)
+                {
+                    conditionSb.Append("AND producerid = @producerid");
+                    paraList.Add(new MySqlParameter("producerid", producerid));
+                }
+
+                if (!string.IsNullOrEmpty(cust_name))
+                {
+                    conditionSb.Append("AND cust_name = @cust_name");
+                    paraList.Add(new MySqlParameter("cust_name", cust_name));
+                }
+
+                if (!string.IsNullOrEmpty(sub_name))
+                {
+                    conditionSb.Append("AND sub_name = @sub_name");
+                    paraList.Add(new MySqlParameter("sub_name", sub_name));
+                }
+
+                #endregion
+
+                #region 排序 分页
+                string orderbyStr = string.Empty;
+                if (!string.IsNullOrEmpty(orderbyCol) && orderby.HasValue)
+                    orderbyStr = orderbyFormat.getSortStr(orderby.Value, orderbyCol);
+                else
+                    orderbyStr = " order by pkid ";
+
+                selectSql += conditionSb.ToString() + orderbyStr;
+                if (pageIndex > 0)
+                {
+                    selectSql += " LIMIT @pageIndex , @pageSize ";
+                    paraList.Add(new MySqlParameter("pageIndex", (pageIndex - 1) * pageSize));
+                    paraList.Add(new MySqlParameter("pageSize", pageSize));
+                }
+                #endregion
+
+                #region 执行
+
+                var resultList = db.Database.SqlQuery<md_druginfo>(selectSql, paraList.ToArray()).ToList();
+                return resultList;
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                error += BaseTool.FormatExceptionMessage(ex);
+                return null;
+            }
+        }
     }
 }
