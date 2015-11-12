86a3608afff26e368fd28253790944f37a6b6b63
kaerluo <1300453233@qq.com>
2015-11-12 16:58:39 +0800

    添加药品搜索api

---
+++ IBll/MasterData/Imd_somebase_info_Bll.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBll.MasterData
+{
+    class Imd_somebase_info_Bll
+    {
+    }
+}
--- webApi/Controllers/MasterData/DruginfoController.cs
+++ webApi/Controllers/MasterData/DruginfoController.cs
@@ -4,15 +4,40 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Model;
+using Bll;
 
 namespace webApi.Controllers.MasterData
 {
-    public class DruginfoController : ApiController
+    public class DruginfoController :BaseApiController
     {
+
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IEnumerable<md_druginfo> Get(int? pageindex)
         {
-            return new string[] { "value1", "value2" };
+            if (pageindex.HasValue == false)
+                pageindex = 1;
+
+            //分组查询
+            string error = string.Empty;
+            md_druginfo_Bll drugbll = new md_druginfo_Bll();
+            var resultlist = drugbll.SearchDruginfoList(null, null, 0, null, null,
+            null, null,  pageindex.Value, PageSize, out error);
+            return resultlist;
+        }
+
+        // GET api/<controller>/5
+        public IEnumerable<md_druginfo> Get(string drugname, int? pageindex)
+        {
+            if (pageindex.HasValue == false)
+                pageindex = 1;
+
+            //分组查询
+            string error = string.Empty;
+            md_druginfo_Bll drugbll = new md_druginfo_Bll();
+            var resultlist = drugbll.SearchDruginfoList(drugname, null, 0, null, null,
+            null, null, pageindex.Value, PageSize, out error);
+            return resultlist;
         }
 
         // GET api/<controller>/5
