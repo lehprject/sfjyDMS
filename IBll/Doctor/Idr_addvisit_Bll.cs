0bc3eb0a257916238ae9e78d8d0724386b7d1ab3
lichao89 <61640542@qq.com>
2015-11-13 15:21:04 +0800

    添加医生加号的Bll DA

---
+++ IBll/Doctor/Idr_addvisit_Bll.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBll.Doctor
+{
+    class Idr_addvisit_Bll
+    {
+    }
+}
