1f933c07dc3eefe604b8f742ff8091271d4bd236
lichao89 <61640542@qq.com>
2015-11-16 14:18:12 +0800

    添加患者留言的controller

---
+++ webApi/Controllers/Patient/PatientMessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bll;
+using Share;
+using Model;
+
+namespace webApi.Controllers.Patient
+{
+    public class PatientMessageController : BaseApiController
+    {
+        public IEnumerable<patient_message> GetListForDoctor(int pageindex)
+        {
+            return null;
+        }
+    }
+}
