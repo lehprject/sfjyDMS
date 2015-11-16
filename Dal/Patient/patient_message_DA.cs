aac3036192c3a85e5badc88e01213e512ac79e76
lichao89 <61640542@qq.com>
2015-11-16 13:45:24 +0800

    留言bll da

---
+++ Dal/Patient/patient_message_DA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Patient
+{
+    class patient_message_DA
+    {
+    }
+}
