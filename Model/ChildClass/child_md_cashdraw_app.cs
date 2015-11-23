df53cc635f50d5f2577c6d1649f16a66612cfb0a
kaerluo <1300453233@qq.com>
2015-11-23 15:31:21 +0800

    添加child_md_cashdraw_app

---
+++ Model/ChildClass/child_md_cashdraw_app.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class md_cashdraw_app
+    {
+        /// <summary>
+        /// 医生姓名
+        /// </summary>
+        public string doctor_name { get; set; }
+
+        /// <summary>
+        /// 所属医院
+        /// </summary>
+        public string hospital { get; set; }
+
+        /// <summary>
+        /// 转入银行
+        /// </summary>
+        public string bank { get; set; }
+    }
+}
