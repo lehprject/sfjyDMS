ed2c8c03bcf585a72e59fe38df3be61e9c89c158
kaerluo <1300453233@qq.com>
2015-11-25 15:19:46 +0800

    添加child_promotion_events

---
+++ Model/ViewModel/vm_SelectInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModel
+{
+    public class vm_SelectInfo
+    {
+        public string id { get; set; }
+
+        public string text { get; set; }
+
+        public Dictionary<string, string> additionitems { get; set; }
+
+    }
+}
